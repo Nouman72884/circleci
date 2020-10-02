5fefbe7cd3d3622f3e8cfdb9df79a9284b368789
Nouman72884 <noumansatti7@gmail.com>
2020-10-03 01:13:33 +0500

    prod

--- modules/infrastructure/modules/security_group/output.tf
+++ modules/infrastructure/modules/security_group/output.tf
@@ -1,6 +1,4 @@
-output "LoadBalancerSecurityGroupId" {
-  value = aws_security_group.LoadBalancerSecurityGroup.id
-}
+
 
 output "InstanceSecurityGroupId" {
   value = aws_security_group.InstanceSecurityGroup.id
